0707b810d228076217d323359c911cfbb3aea345
Lucas Frezarini <lucas.frezarini@gmail.com>
2018-05-22 23:03:24 -0300

    Consertando a sidebar de categoria

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router";
 
 class CategoryList extends Component {
 	render() {
@@ -7,65 +8,32 @@ class CategoryList extends Component {
         <div className="panel panel-default">
           <div className="panel-heading">
             <h4 className="panel-title">
-              <a data-toggle="collapse" data-parent="#accordian" href="#sportswear">
+              <Link to="/products?category=Trajes">
                 <span className="badge pull-right"><i className="fa fa-plus"></i></span>
                 Trajes
-              </a>
+              </Link>
             </h4>
-          </div>
-          <div id="sportswear" className="panel-collapse collapse">
-            <div className="panel-body">
-              <ul>
-                <li><a>Yi-Fus </a></li>
-                <li><a>Uniformes Tradicionais </a></li>
-                <li><a>Trajes de Combate </a></li>
-                <li><a>Faixas</a></li>
-              </ul>
-            </div>
           </div>
         </div>
         <div className="panel panel-default">
           <div className="panel-heading">
             <h4 className="panel-title">
-              <a data-toggle="collapse" data-parent="#accordian" href="#mens">
+              <Link to="/products?category=Armas">
                 <span className="badge pull-right"><i className="fa fa-plus"></i></span>
                 Armas
-              </a>
+              </Link>
             </h4>
-          </div>
-          <div id="mens" className="panel-collapse collapse">
-            <div className="panel-body">
-              <ul>
-                <li><a>Bastões</a></li>
-                <li><a>Lançãs</a></li>
-                <li><a>Armas Cortantes</a></li>
-                <li><a>Armas Articuladas</a></li>
-                <li><a>Armas do Jackie Chan</a></li>
-                <li><a>Armas Saotome</a></li>
-              </ul>
-            </div>
           </div>
         </div>
         
         <div className="panel panel-default">
           <div className="panel-heading">
             <h4 className="panel-title">
-              <a data-toggle="collapse" data-parent="#accordian" href="#womens">
+              <Link to="/products?category=Equipamentos">
                 <span className="badge pull-right"><i className="fa fa-plus"></i></span>
                 Equipamentos
-              </a>
+              </Link>
             </h4>
-          </div>
-          <div id="womens" className="panel-collapse collapse">
-            <div className="panel-body">
-              <ul>
-                <li><a>Luvas</a></li>
-                <li><a>Ataduras</a></li>
-                <li><a>Protetores</a></li>
-                <li><a>Aparadores</a></li>
-                <li><a>Sacos de Pancada</a></li>
-              </ul>
-            </div>
           </div>
         </div>
         
